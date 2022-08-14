281ea941a86edfaa3d2535956b00fc698bd4dc6a
yoannakr <jkrmilkova@gmail.com>
2022-08-14 22:53:54 +0300

    Added about section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.scss";
+import About from "./components/About/About";
 import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
       <Home />
+      <NavBar />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "../../App.scss";
+import NavBar from "../NavBar/NavBar";
+import styles from "./About.module.scss";
+
+const About = () => {
+  return (
+    <div id="about" className={styles.About}>
+      <div className={styles.AboutWrapper}>
+        <div className={styles.ProfilePicture}>
+          <img src="images/profile-picture.jpg" />
+        </div>
+        <div className={styles.AboutDescription}>
+          <p className={styles.Title}>Hello, I am Yoanna Krasteva 🙂</p>
+          <p>
+            I am passionate about coding and solving problems through code. A
+            person who cares about the details.
+          </p>
+          <p>
+            I've recently graduated Bachelor's degree in Computer and Software
+            Engineering at Technical University of Sofia. Currently, I am
+            working both front end and back end at Microinvest.
+          </p>
+          <p>
+            I enjoy figuring out the logic behind creating applications but I am
+            more interested in learning how to make it look pretty to users.
+            That's why I want to switch my career in front end development.
+          </p>
+          <a
+            href="Yoanna_Krasteva_Software_Engineer.pdf"
+            className={styles.DownloadButton}
+            download
+          >
+            Download my resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
